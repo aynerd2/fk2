acf2a8eb138cbceb203af02bdcdb6453a713e128
Ayobami <olawaleayowande@gmail.com>
2024-08-05 12:10:02 +0100

    model added

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 import os
 from flask import Flask, request, jsonify
-from transformers import AutoModelForImageClassification, AutoProcessor
-import torch
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import img_to_array, load_img
+import numpy as np
 from PIL import Image
 
 app = Flask(__name__)
 
-model_id = "Aynerd/NiaraApi"
-model = AutoModelForImageClassification.from_pretrained(model_id)
-processor = AutoProcessor.from_pretrained(model_id)
+# Path to the .h5 model file
+model_path = r'./model/model.h5'
+  # Update this path
+model = load_model(model_path)
 
 UPLOADS_FOLDER = '/tmp/uploads'
 
@@ -26,19 +28,19 @@ def predict():
         filepath = os.path.join(UPLOADS_FOLDER, file.filename)
         file.save(filepath)
 
-        image = Image.open(filepath)
-        inputs = processor(images=image, return_tensors="pt")
+        # Load and preprocess the image
+        image = load_img(filepath, target_size=(150, 150))  # Adjust target_size as needed
+        img_array = img_to_array(image)
+        img_array = np.expand_dims(img_array, axis=0) / 255.0
 
-        with torch.no_grad():
-            outputs = model(**inputs)
-            predictions = outputs.logits
-
-        predicted_class = torch.argmax(predictions)
-        confidence = torch.max(predictions)
+        # Predict using the model
+        predictions = model.predict(img_array)
+        predicted_class = np.argmax(predictions[0])
+        confidence = float(np.max(predictions[0]))
 
         os.remove(filepath)
 
-        return jsonify({'class': predicted_class.item(), 'confidence': confidence.item()})
+        return jsonify({'class': int(predicted_class), 'confidence': confidence})
 
     return jsonify({'error': 'File not readable'}), 400
 
